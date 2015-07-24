befe85ee3b284622840f23e6399f2fe6dfb826dd
AndrezaSB <andrezadesouzabrito@gmail.com>
2015-07-24 13:52:08 -0300

    SYSOUT

--- Treinamento_GIT/src/fofinho/Git.java
+++ Treinamento_GIT/src/fofinho/Git.java
@@ -9,14 +9,9 @@ public class Git {
 
 		System.out.println("Anderson We Read");
 
-<<<<<<< HEAD
 		System.out.println("Andreza");
-<<<<<<< HEAD
-		System.out.println("Fofinhooooo");
-=======
 
->>>>>>> 452d7ff47f99aaff3628d717d1bac2ef481e8cab
-=======
->>>>>>> 4bd6a308dbf702831122c3c42badda201e6e1e82
+		System.out.println("Fofinhooooo");
+
 	}
 }
